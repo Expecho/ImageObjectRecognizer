cbb8b47dbe54aa692883645bbea6a7185d697f28
Expecho <github@2000miles.nl>
2019-03-08 18:55:32 +0100

    Document extension method

--- src/ImageObjectRecognizer/ServiceCollectionServiceExtensions.cs
+++ src/ImageObjectRecognizer/ServiceCollectionServiceExtensions.cs
@@ -6,12 +6,29 @@ namespace ImageMetadataUpdater
 {
     internal static class ServiceCollectionServiceExtensions
     {
-        public static IServiceCollection AddSingletonUsingTypeString<TService>(this IServiceCollection services, string type)
+        // Summary:
+        //     Adds a singleton service of the type specified in serviceType with an implementation
+        //     of the type with the name as specified in typeName to the specified Microsoft.Extensions.DependencyInjection.IServiceCollection.
+        //
+        // Parameters:
+        //   services:
+        //     The Microsoft.Extensions.DependencyInjection.IServiceCollection to add the service
+        //     to.
+        //
+        //   serviceType:
+        //     The type of the service to register.
+        //
+        //   typeName:
+        //     The name of the implementation type of the service.
+        //
+        // Returns:
+        //     A reference to this instance after the operation has completed.
+        public static IServiceCollection AddSingletonUsingTypeString<TService>(this IServiceCollection services, string typeName)
         {
             var implementationType = Assembly.GetExecutingAssembly()
                 .GetTypes()
                 .Where(t => t.GetInterface(typeof(TService).FullName) != null)
-                .FirstOrDefault(t => t.Name == type);
+                .FirstOrDefault(t => t.Name == typeName);
 
             services.AddSingleton(typeof(TService), implementationType);
 
